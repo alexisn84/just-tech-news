81bf6e8ad7fd2be05f3731c83395d8e06bbcdc2a
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-16 13:46:21 -0500

    save feature in session broke code cannot find solution

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,34 +1,3 @@
-//function to operate submit button for signup form
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    //fetch request to api/users to POST username/email/password from form to server
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        //use await b4 the Promise
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        //console.log(response);
-        //check if response status
-        if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
-//function to operate submit button and tasks for login 
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -37,24 +6,48 @@ async function loginFormHandler(event) {
 
     if (email && password) {
         const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
+        method: 'post',
+        body: JSON.stringify({
+            email,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
         });
-        //check if response ok
+
         if (response.ok) {
-            console.log('success');
+        document.location.replace('/');
         } else {
-            alert(response.statusText);
+        alert(response.statusText);
         }
     }
 }
 
-//listener for signup button
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+async function signupFormHandler(event) {
+    event.preventDefault();
 
-//listener for login button
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+            username,
+            email,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+        document.location.replace('/');
+        } else {
+        alert(response.statusText);
+        }
+    }
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const { dirname } = require('path/posix');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //set up handlebars as apps template of choice
 const exphbs = require('express-handlebars');
@@ -10,6 +13,16 @@ const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +34,9 @@ app.set('view engine', 'handlebars');
 
 //turn on routes
 app.use(routes);
+
+//turn on session
+app.use(session(sess));
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
