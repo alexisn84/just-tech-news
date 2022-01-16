e700f90ca28d33bcda6d428a3205b0fdd2cfac78
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-16 17:06:14 -0500

    found error preventing login corrected

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const { dirname } = require('path/posix');
+const express = require('express');
 const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-//set up handlebars as apps template of choice
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -24,21 +19,19 @@ const sess = {
     })
 };
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//add public folder
-app.use(express.static(path.join(__dirname, 'public')));
-//set handlbars to be engine
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-//turn on routes
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//turn on session
-app.use(session(sess));
+app.use(require('./controllers/'));
 
-//turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
