45f03ab47892c28cbf83782de56a97a3f7d49406
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-17 10:26:58 -0500

    hide login/logout pending if user is logged in or not

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,10 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             // pass post object into the homepage template
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+            res.render('homepage', 
+            { posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
@@ -78,7 +81,7 @@ router.get('/post/:id', (req, res) => {
         const post = dbPostData.get({ plain: true });
 
         //pass data to template
-        res.render('single-post', { 
+        res.render('single-post', {
             post,
             loggedIn: req.session.loggedIn
         });
