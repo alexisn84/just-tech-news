24fbde00daf7c0d4152c2102270fe268361fa627
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-19 12:55:44 -0500

    add new posts ability, feature is not working

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,10 @@ const res = require('express/lib/response');
 const apiRoutes = require('./api');
 
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 // router.use((req, res) => {
